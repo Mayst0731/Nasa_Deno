c042f8c994e195ca318c324fb4a93d863a0d70c7
apple <xiaomei6@illinois.edu>
2020-09-02 21:33:08 -0500

    add logging to api

--- mod.ts
+++ mod.ts
@@ -1,12 +1,27 @@
-import { Application,send } from "https://deno.land/x/oak@v6.0.1/mod.ts";
+// @ts-nocheck
+import * as log from "https://deno.land/std/log/mod.ts";
+
+import { Application, send } from "https://deno.land/x/oak@v6.0.1/mod.ts";
 import api from "./api.ts";
 
 const app = new Application();
 const PORT = 8000;
 
+await log.setup({
+    handlers: {
+        console: new log.handlers.ConsoleHandler("INFO"),
+    },
+    loggers: {
+        default: {
+            level: "INFO",
+            handlers: ["console"],
+        }
+    }
+});
+
 app.use(async (ctx,next) => { 
     await next();
-    console.log(`${ctx.request.method} ${ctx.request.url}`);
+    log.info(`${ctx.request.method} ${ctx.request.url}`);
 });
 
 app.use(api.routes());
@@ -29,6 +44,7 @@ app.use(async (ctx) => {
 
 
 if (import.meta.main) { 
+    log.info(`Starting server on port ${PORT}......`);
     await app.listen({
         port: PORT,
     });
